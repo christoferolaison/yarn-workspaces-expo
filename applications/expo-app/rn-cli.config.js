c5f9c04513673af478bb6dd65c1b0bd8e3015948
Christofer Olaison <christofer.olaison@mentimeter.com>
2018-11-11 19:14:20 +0100

    no need for this

--- applications/expo-app/rn-cli.config.js
+++ applications/expo-app/rn-cli.config.js
@@ -2,8 +2,7 @@ const path = require("path");
 const getWorkspaces = require("get-yarn-workspaces");
 const blacklist = require("metro-config/src/defaults/blacklist");
 const workspaces = getWorkspaces(__dirname);
-const rootNodeModules = path.resolve(__dirname, "..", "..", "node_modules");
-console.log({ workspaces, rootNodeModules });
+
 const config = {
   resolver: {
     extraNodeModules: {
@@ -11,19 +10,6 @@ const config = {
       react: path.resolve(__dirname, "node_modules/react"),
       "@babel/runtime": path.resolve(__dirname, "node_modules/@babel/runtime")
     },
-    blacklistRE: blacklist([
-      `/${rootNodeModules.replace(
-        /\//g,
-        "[/\\\\]"
-      )}[/\\\\]node_modules[/\\\\]react-native[/\\\\].*/`,
-      ...workspaces.map(
-        workspacePath =>
-          `/${workspacePath.replace(
-            /\//g,
-            "[/\\\\]"
-          )}[/\\\\]node_modules[/\\\\]react-native[/\\\\].*/`
-      )
-    ]),
     projectRoot: path.resolve(__dirname),
     watchFolders: [
       path.resolve(__dirname, "..", "..", "node_modules"),
@@ -31,5 +17,4 @@ const config = {
     ]
   }
 };
-console.log({ config });
 module.exports = config;
